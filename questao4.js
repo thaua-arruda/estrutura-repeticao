e00d83e151fc9b568f4427b64eb4af7a8d29e2b6
thaua-arruda <thauafarruda@gmail.com>
2024-04-03 16:36:25 -0400

    questao 4 feita

---
+++ questao4.js
@@ -0,0 +1,8 @@
+/*
+4. Questão: Escreva um código que imprima os números de 0 a 100 em 
+incrementos de 10.
+*/
+
+for (let i = 10; i <= 100; i += 10) {
+      console.log("Estes são os números= " + i)
+}
