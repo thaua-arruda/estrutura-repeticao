f448030cac8d766229fe2ae762a3bc4d468e6f4f
thaua-arruda <thauafarruda@gmail.com>
2024-04-03 16:36:14 -0400

    questao 3 feita

---
+++ questao3.js
@@ -0,0 +1,8 @@
+/*
+3. Questão: Escreva um código que imprima os números de 10 a 1 em 
+ordem decrescente.
+*/
+
+for (let decres = 10; decres >= 1; decres--) {
+    console.log("Números decrescentes ordenados: " + decres)
+}
