28bf4627481396d6638c0885e28db338152808f8
thaua-arruda <thauafarruda@gmail.com>
2024-04-03 16:37:10 -0400

    questao 10 feita

---
+++ questao10.js
@@ -0,0 +1,11 @@
+/*
+10. Questão: Escreva um código que imprima a tabuada do 5 (de 1 a 10)
+*/
+
+let i = 0;
+
+while ( i <= 10) {
+    let resultado = 5 * i; 
+    console.log("5 x "+ i +"= "+ resultado);
+    i++
+}
